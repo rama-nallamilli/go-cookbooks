d044a0e344a3f768963a431a9c3c5f7d03a46f0b
Rama Nallamilli <rama.nallamilli@gmail.com>
2014-05-30 19:07:41 +0100

    bds-318

--- cookbooks/go-server/solo.rb
+++ cookbooks/go-server/solo.rb
@@ -2,4 +2,4 @@ root = File.absolute_path(File.dirname(__FILE__))
 
 file_cache_path root
 cookbook_path root + '/../'
-environment_path = root + '/environments/'
+environment_path root + '/environments/'
